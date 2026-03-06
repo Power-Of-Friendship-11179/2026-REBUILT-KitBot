aac964d87c829e132f9f5f0c8b2f1e94d3d1cb8a
Steve Wasleski <sfwasleski@att.net>
2026-03-05 20:15:13 -0500

    Keep shooter at speed and agitate more often

--- src/main/java/frc/robot/auto/ShootPreloadsSequence.java
+++ src/main/java/frc/robot/auto/ShootPreloadsSequence.java
@@ -1,16 +1,26 @@
 package frc.robot.auto;
 
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.Constants.FuelConstants;
 import frc.robot.commands.Agitate;
-import frc.robot.commands.LaunchSequence;
+import frc.robot.commands.Launch;
+import frc.robot.commands.SpinUp;
 import frc.robot.subsystems.CANFuelSubsystem;
 import frc.robot.subsystems.CANShooter;
 
-public class ShootPreloadsSequence extends SequentialCommandGroup {
+public class ShootPreloadsSequence extends ParallelCommandGroup {
     public ShootPreloadsSequence(CANFuelSubsystem ballSubsystem, CANShooter shooterSubsystem) {
         addCommands(
-                new LaunchSequence(ballSubsystem, shooterSubsystem).withTimeout(6.0),
-                new Agitate(ballSubsystem).withTimeout(3.0),
-                new LaunchSequence(ballSubsystem, shooterSubsystem).withTimeout(6.0));
+                shooterSubsystem.shoot(),
+                new SequentialCommandGroup(
+                        new SpinUp(ballSubsystem).withTimeout(FuelConstants.SPIN_UP_SECONDS),
+                        new Launch(ballSubsystem).withTimeout(3.0),
+                        new Agitate(ballSubsystem).withTimeout(2.0),
+                        new Launch(ballSubsystem).withTimeout(3.0),
+                        new Agitate(ballSubsystem).withTimeout(2.0),
+                        new Launch(ballSubsystem).withTimeout(3.0),
+                        new Agitate(ballSubsystem).withTimeout(2.0),
+                        new Launch(ballSubsystem).withTimeout(3.0)));
     }
 }
